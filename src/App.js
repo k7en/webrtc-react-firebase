7a531afd0264f09e520c4fc5db66f997262a6883
鈴木健一 <kensuzuki@MacBook-Air.local>
2022-04-12 08:28:36 +0900

    permit audio and video

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React from 'react';
 
-function App() {
+const getMedia = async() => {
+  const constraints = {audio: true, video: true}
+
+  try {
+    return await navigator.mediaDevices.getUserMedia(constraints);
+    /* ストリームを使用 */
+  } catch(err) {
+    /* エラーを処理 */
+    console.log(err)
+  }
+}
+
+getMedia()
+
+const  App = () => {
   return <div>Hello, React!</div>;
 }
 
