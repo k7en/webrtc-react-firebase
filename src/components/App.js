916f42cfa54b5193c1919e8bf2d9ae0a262dfdbe
Ken Suzuki <kenichi.suzuki@evan-tec.co.jp>
2022-04-13 08:13:15 +0900

    InputFormRemote

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import InputFormLocal from './InputFormLocal'
 import InputFormRemote from './InputFormRemote'
 
@@ -17,10 +17,18 @@ const getMedia = async() => {
 getMedia()
 
 const  App = () => {
-  return <>
-  <InputFormLocal />
-  <InputFormRemote />
-  </>;
+  const [localPeerName, setLocalPeerName] = useState('');
+  const [remotePeerName, setRemotePeerName] = useState('');
+  return( <>
+  <InputFormLocal
+    localPeerName={localPeerName}
+    setLocalPeerName={setLocalPeerName}
+  />
+  <InputFormRemote
+    remotePeerName={remotePeerName}
+    setRemotePeerName={setRemotePeerName}
+  />
+  </>);
 }
 
 export default App;
--- src/components/InputFormRemote.js
+++ src/components/InputFormRemote.js
@@ -3,10 +3,7 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
@@ -17,8 +14,8 @@ function Copyright(props) {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
+      <Link color="inherit" href="https://www.google.com">
+       けんいち
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
@@ -29,7 +26,7 @@ function Copyright(props) {
 const theme = createTheme();
 
 export default function SignIn() {
-  if (true) return <></>;
+  const label = '相手の名前'
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
@@ -51,57 +48,26 @@ export default function SignIn() {
             alignItems: 'center',
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
           <Typography component="h1" variant="h5">
-            Sign in
+            {label}を入力してください
           </Typography>
           <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
             <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
               autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
               fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
+              label={label}
+              margin="normal"
+              name="name"
+              required
             />
             <Button
-              type="submit"
               fullWidth
-              variant="contained"
               sx={{ mt: 3, mb: 2 }}
+              type="submit"
+              variant="contained"
             >
-              Sign In
+              決定
             </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="#" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
           </Box>
         </Box>
         <Copyright sx={{ mt: 8, mb: 4 }} />
