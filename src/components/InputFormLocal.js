93fcd4a98b380f02347ef360204e26a1ef93f0cc
Ken Suzuki <kenichi.suzuki@evan-tec.co.jp>
2022-04-15 12:03:40 +0900

    input form remote fixed.

--- src/components/InputFormLocal.js
+++ src/components/InputFormLocal.js
@@ -7,7 +7,6 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { ClassNames } from "@emotion/react";
 
 function Copyright(props) {
   return (
--- src/components/InputFormRemote.js
+++ src/components/InputFormRemote.js
@@ -1,14 +1,13 @@
-import * as React from "react";
-import Avatar from "@mui/material/Avatar";
+import React, { useCallback, useEffect, useState } from "react";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
 import Link from "@mui/material/Link";
 import Box from "@mui/material/Box";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { makeStyles } from "@material-ui/core/styles";
 
 function Copyright(props) {
   return (
@@ -28,10 +27,39 @@ function Copyright(props) {
   );
 }
 
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
 const theme = createTheme();
 
-export default function SignIn({ remotePeerName, setRemotePeerName }) {
+export default function SignIn({
+  localPeerName,
+  remotePeerName,
+  setRemotePeerName,
+}) {
   const label = "相手の名前";
+  const classes = useStyles();
+  const [disabeld, setDisabled] = useState(true);
+  const [name, setName] = useState("");
+  const [isComposed, setIsComposed] = useState(false);
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
@@ -40,6 +68,22 @@ export default function SignIn({ remotePeerName, setRemotePeerName }) {
       password: data.get("password"),
     });
   };
+
+  useEffect(() => {
+    const disabled = name === "";
+    setDisabled(disabled);
+  }, [name]);
+
+  const initializeRemotePeer = useCallback(
+    (e) => {
+      e.preventDefault();
+      setRemotePeerName(name);
+    },
+    [name, setRemotePeerName]
+  );
+
+  if (localPeerName === "") return <></>;
+  if (remotePeerName !== "") return <></>;
 
   return (
     <ThemeProvider theme={theme}>
@@ -68,13 +112,27 @@ export default function SignIn({ remotePeerName, setRemotePeerName }) {
               label={label}
               margin="normal"
               name="name"
+              onChange={(e) => setName(e.target.value)}
+              onCompositionEnd={() => setIsComposed(false)}
+              onCompositionStart={() => setIsComposed(true)}
+              onKeyDown={(e) => {
+                if (e.target.value === "") return;
+                if (isComposed) return;
+                if (e.key === "Enter") {
+                  initializeRemotePeer(e);
+                }
+              }}
               required
+              value={name}
             />
             <Button
+              className={classes.submit}
               fullWidth
               sx={{ mt: 3, mb: 2 }}
               type="submit"
               variant="contained"
+              disabled={disabeld}
+              onClick={(e) => initializeRemotePeer(e)}
             >
               決定
             </Button>
