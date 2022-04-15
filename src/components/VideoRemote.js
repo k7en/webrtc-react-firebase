b39751573210474ccf0048f19ca21ea1006667c0
Ken Suzuki <kenichi.suzuki@evan-tec.co.jp>
2022-04-15 13:10:29 +0900

    fixed remote view

--- src/components/VideoRemote.js
+++ src/components/VideoRemote.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import Video from "./Video";
 
-const VideoRemote = () => {
-  return <></>;
+const VideoRemote = ({ name }) => {
+  const videoRef = null;
+  return <Video isLocal={false} name={name} videoRef={videoRef} />;
 };
 
 export default VideoRemote;
