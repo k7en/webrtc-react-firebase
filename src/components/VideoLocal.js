33bbb9ea87dcbc3d1c61bfd3cb46dbdac2150992
Ken Suzuki <kenichi.suzuki@evan-tec.co.jp>
2022-04-16 15:46:45 +0900

    getUserMediastrem to rtcclient

--- src/components/VideoLocal.js
+++ src/components/VideoLocal.js
@@ -1,29 +1,26 @@
 import React, { useEffect, useRef } from "react";
 import Video from "./Video";
 
-const VideoLocal = ({ name }) => {
+const VideoLocal = ({ rtcClient }) => {
   const videoRef = useRef(null);
   const currentVideoRef = videoRef.current;
+  const mediaStream = rtcClient.mediaStream;
 
   useEffect(() => {
     if (currentVideoRef === null) return;
-
-    const getMedia = async () => {
-      const constraints = { audio: true, video: true };
-
+    const getMedia = () => {
       try {
-        const mediaStream = await navigator.mediaDevices.getUserMedia(
-          constraints
-        );
-        currentVideoRef.srcObject = mediaStream;
+        currentVideoRef.srcObject = rtcClient.mediaStream;
       } catch (err) {
         console.log(err);
       }
     };
 
     getMedia();
-  }, [currentVideoRef]);
-  return <Video isLocal={true} name={name} videoRef={videoRef} />;
+  }, [currentVideoRef, mediaStream]);
+  return (
+    <Video isLocal={true} name={rtcClient.localPeerName} videoRef={videoRef} />
+  );
 };
 
 export default VideoLocal;
