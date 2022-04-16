e5979ea745346ec7c28620a90a54ae49cfb16bad
Ken Suzuki <kenichi.suzuki@evan-tec.co.jp>
2022-04-16 16:43:26 +0900

    install firebase

---
+++ src/utils/FirebaseSignalingClient.js
@@ -0,0 +1,26 @@
+import { FoodBank } from "@mui/icons-material";
+import firebase from "firebase/app";
+
+// import "firebase/auth"
+import "firebase/database";
+
+export default class FirebaseSignalingClient {
+  constructor() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyDtq2L_tH6hmYDgzGnyRAWfo3hK_s9xnPk",
+      authDomain: "webrtc-react-firebase-ken.firebaseapp.com",
+      databaseURL:
+        "https://webrtc-react-firebase-ken-default-rtdb.firebaseio.com",
+      projectId: "webrtc-react-firebase-ken",
+      storageBucket: "webrtc-react-firebase-ken.appspot.com",
+      messagingSenderId: "720405174765",
+      appId: "1:720405174765:web:1185b321ffe30457932fe4",
+    };
+    if (firebase.apps.length === 0) {
+      firebase.initializeApp(firebaseConfig);
+    }
+    this.database = firebase.database();
+    this.localPeerName = "";
+    this.remotePeerName = "";
+  }
+}
--- src/utils/RtcClient.js
+++ src/utils/RtcClient.js
@@ -1,9 +1,12 @@
+import FirebaseSignalingClient from "./FirebaseSignalingClient";
+
 export default class RtcClient {
   constructor(setRtcClient) {
     const config = {
       iceServers: [{ urls: "stun:stun.stunprotocol.org" }],
     };
     this.rtcPeerConnection = new RTCPeerConnection(config);
+    this.FirebaseSignalingClient = new FirebaseSignalingClient();
     this.localPeerName = "";
     this.remotePeerName = "";
     this._setRtcClient = setRtcClient;
@@ -25,6 +28,11 @@ export default class RtcClient {
   startListening(localPeerName) {
     this.localPeerName = localPeerName;
     this.setRtcClient();
-    // TODO: ここでシグナリングサーバーをリスんする。
+    // ここでシグナリングサーバーをListenする。https://firebase.google.com/docs/database/web/read-and-write?hl=ja#web_value_events
+    this.FirebaseSignalingClient.database
+      .ref(localPeerName)
+      .on("value", (snapshot) => {
+        const data = snapshot.val();
+      });
   }
 }
