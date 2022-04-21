c961ff16165cceed8fa63110d1e9ebea05cbdd62
Ken Suzuki <kenichi.suzuki@evan-tec.co.jp>
2022-04-21 22:35:10 +0900

    refine video components

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,11 +1,18 @@
 import React from "react";
 
+import { Card, CardActions, CardContent, Typography } from "@mui/material";
+
 const Video = ({ isLocal, name, videoRef }) => {
   return (
-    <div>
+    <Card sx={{ maxWidth: 345 }}>
       <video autoPlay muted={isLocal} ref={videoRef} />
-      {name}
-    </div>
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {name}
+        </Typography>
+      </CardContent>
+      <CardActions></CardActions>
+    </Card>
   );
 };
 
