bcc435bc5bf2281662e5abbd259e1d05c49e31ef
Ken Suzuki <kenichi.suzuki@evan-tec.co.jp>
2022-04-23 21:36:36 +0900

    change video width dynamicaly

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useRef } from "react";
 
 import { Card, CardActions, CardContent, Typography } from "@mui/material";
 
+import useDimentions from "./hooks/useDimentions";
+
 const Video = ({ isLocal, name, videoRef }) => {
+  const refCard = useRef(null);
+  const dimentionsCard = useDimentions(refCard);
+
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <video autoPlay muted={isLocal} ref={videoRef} />
+    <Card ref={refCard}>
+      <video
+        autoPlay
+        muted={isLocal}
+        ref={videoRef}
+        // width={300}
+        width={dimentionsCard.width}
+      />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
           {name}
